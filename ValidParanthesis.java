0bdaa7617b38a8ec774744209626ce5a218e45ff
Mohammad Shahadath <shahadsami9795@gmail.com>
2026-05-27 23:10:33 +0530

    Valid Paranthesis

---
+++ ValidParanthesis.java
@@ -0,0 +1,24 @@
+package practise;
+import java.util.Stack;
+public class ValidParanthesis {
+
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='('){
+                stack.add(')');
+            }else if(c=='{'){
+                stack.add('}');
+            }else if(c=='['){
+                stack.add(']');
+            }else{
+                if(stack.isEmpty() || stack.pop()!=c){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+        
+    }
+}
